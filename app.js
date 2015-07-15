48ad57a545a84edf8412e511fca8d5c5913e7fb1
MSwarz <sinnyck@hotmail.com>
2015-07-16 00:37:53 +0200

    Primera página y favicon

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var path = require('path');
+// importamos paquetes con middlewares, instalados con npm install tras leer json
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//importamos enrutadores
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
+// no vamos a importar users
+//var users = require('./routes/users');
 
 var app = express();
 
@@ -15,16 +19,24 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+//instalamos midlewares que impotamos antes
+//de login, analisis de json, ..., cookies, ...
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//instalamos los enrutadores
+app.use('/', routes); // asocia las rutas a los gestores
 
+// no usamos  este
+//app.use('/users', users);
+
+//este midleware para responder a cualquier otra ruta no atendida
+// por enrutadores anteriores: genera error 404 de http
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -52,9 +64,9 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {}
+        error: {} // no imprime el error
     });
 });
 
-
+//exportar app para comando de arranque
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router(); // generamos un enrutador basico
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });
 });
 
 module.exports = router;
