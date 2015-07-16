25336a53db1c37ba930a8cfc8d4448e18752e670
MSwarz <sinnyck@hotmail.com>
2015-07-16 23:45:28 +0200

    Marco y CSS adaptable

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var partials = require('express-partials');
 //importamos enrutadores
 var routes = require('./routes/index');
 
@@ -18,16 +18,19 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
-//instalamos midlewares que impotamos antes
+
+//instalamos midlewares que importamos antes:
 //de login, analisis de json, ..., cookies, ...
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());
 
 //instalamos los enrutadores
 app.use('/', routes); // asocia las rutas a los gestores
