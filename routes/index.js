f34685abfa5bed5bd41dafbe5fce19c7168e2a93
MSwarz <sinnyck@hotmail.com>
2015-07-18 19:53:14 +0200

    enlaces y rama creditos

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+//atender peticiones
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
+router.get('/author', function(req, res) {
+  res.render('author', { hechopor: 'Manuel Suárez' });
+});
+
 
 
 module.exports = router;
