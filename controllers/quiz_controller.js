7a38cbd1a4f3c96a0f3394d5b4403ed54fc4a530
MSwarz <sinnyck@hotmail.com>
2015-07-19 20:33:23 +0200

    Bases de datos sqlite y postgress heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,21 @@
+//el controlador importa el modelo para acceder a la BD
+var models = require('../models/models.js');
+
 /* GET quizes/question */
 exports.question = function(req, res) {
-  res.render('quizes/question', { pregunta: 'Capital de Italia' });
+  models.Quiz.findAll().success(function(quiz) {
+    res.render('quizes/question', { pregunta: quiz[0].pregunta })
+})      //*****************
 };
 
 /* GET quizes/answer */
 exports.answer = function(req, res) {
-  if (req.query.respuesta.toLowerCase() === 'roma'){
-    res.render('quizes/answer',{ respuesta: 'Correcta'})
+  models.Quiz.findAll().success(function(quiz) {
+  if (req.query.respuesta.toLowerCase() === quiz[0].respuesta.toLowerCase()) {
+    res.render('quizes/answer',{ respuesta: 'Correcta'});
   }
   else {
-    res.render('quizes/answer',{ respuesta: 'Incorrecta'})
+    res.render('quizes/answer',{ respuesta: 'Incorrecta'});
   }
+})
 };
